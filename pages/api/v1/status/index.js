b676c8d1168daf8405f9ab8b292238e9031a788a
Fabricio Filipe Viapiana <fabriciofviapiana@gmail.com>
2025-06-12 08:05:35 -0300

    finish /status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,30 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("select 1 + 1");
-  console.log(result.rows);
+  const pgVersionQuery = await database.query("SHOW server_version;");
+  const pgVersion = pgVersionQuery.rows[0].server_version;
+
+  const pgMaxConnQuery = await database.query("SHOW max_connections;");
+  const pgMaxConn = pgMaxConnQuery.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const pgConnCountQuery = await database.query({
+    text: "select count(*)::int from pg_stat_activity WHERE datname = $1",
+    values: [databaseName],
+  });
+  const pgConnCount = pgConnCountQuery.rows[0].count;
+
+  const updatedAt = new Date().toISOString();
+
   response.status(200).json({
-    chave: "test",
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: pgVersion,
+        max_connections: parseInt(pgMaxConn),
+        opened_connections: pgConnCount,
+      },
+    },
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,24 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  console.log(responseBody);
+
+  const parsedDate = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toBe(parsedDate);
+
+  //pg_version
+  expect(responseBody.dependencies.database.version).toBeDefined();
+  expect(responseBody.dependencies.database.version).toContain("16.0");
+
+  //pg_max_conn
+  expect(responseBody.dependencies.database.max_connections).toBeDefined();
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+
+  //pg_count_conn
+  expect(responseBody.dependencies.database.opened_connections).toBeDefined();
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
