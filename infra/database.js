c4a301501a90fab0b0cb55cff1d60648d90b8c38
Fabricio Filipe Viapiana <fabriciofviapiana@gmail.com>
2025-06-18 07:40:44 -0300

    add api/v1/migrations endpoint

--- infra/database.js
+++ infra/database.js
@@ -22,18 +22,9 @@ import { Pool, Client } from "pg";
 // }
 
 async function query(queryObject) {
-  console.log("SSL values", getSSLValues());
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    ssl: getSSLValues(),
-  });
-
+  let client;
   try {
-    await client.connect();
+    client = await getNewClient();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
@@ -51,11 +42,25 @@ const getSSLValues = () => {
     };
   }
 
-  if (process.env.NODE_ENV === "development") return false;
+  if (process.env.NODE_ENV === "production") return true;
 
-  return true;
+  return false;
 };
 
+async function getNewClient() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),
+  });
+  await client.connect();
+  return client;
+}
+
 export default {
-  query: query,
+  query,
+  getNewClient,
 };
---
+++ jest.config.js
@@ -0,0 +1,15 @@
+const nextJest = require("next/jest");
+const dotenv = require("dotenv");
+
+dotenv.config({
+  path: ".env.development",
+  dir: ".",
+});
+
+const createJestConfig = nextJest();
+
+const jestConfig = createJestConfig({
+  moduleDirectories: ["node_modules", "<rootDir>"],
+});
+
+module.exports = jestConfig;
