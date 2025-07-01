07868272360db0863ea0eb2849bed1e8574f181c
Fabricio Filipe Viapiana <fabriciofviapiana@gmail.com>
2025-07-01 08:59:39 -0300

    add `orchestrator.js`

---
+++ tests/orchestrator.js
@@ -0,0 +1,26 @@
+import retry from "async-retry";
+
+async function waitForAllServices() {
+  await waitForWebServer();
+
+  async function waitForWebServer() {
+    return retry(fetchStatusPage, {
+      retries: 100,
+      maxTimeout: 1000,
+      onRetry: (error, attempt) => {
+        console.log(`Error ${error.message} occurred on attempt ${attempt}`);
+      },
+    });
+
+    async function fetchStatusPage() {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      if (!response.ok) {
+        throw new Error(`v1/status is not ok`);
+      }
+    }
+  }
+}
+
+export default {
+  waitForAllServices,
+};
