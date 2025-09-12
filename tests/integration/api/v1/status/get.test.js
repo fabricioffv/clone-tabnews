ea6cd83e6b06183764bfed5bf17abda2519c4f29
Fabricio Viapiana <fabriciofviapiana@gmail.com>
2025-09-12 08:52:09 -0300

    test: better tests descriptians

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,25 +4,31 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+describe("GET /api/v1/status", () => {
+  describe("anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
-  expect(responseBody.updated_at).toBeDefined();
+      const responseBody = await response.json();
+      expect(responseBody.updated_at).toBeDefined();
 
-  const parsedDate = new Date(responseBody.updated_at).toISOString();
-  expect(responseBody.updated_at).toBe(parsedDate);
+      const parsedDate = new Date(responseBody.updated_at).toISOString();
+      expect(responseBody.updated_at).toBe(parsedDate);
 
-  //pg_version
-  expect(responseBody.dependencies.database.version).toBeDefined();
-  expect(responseBody.dependencies.database.version).toContain("16.0");
+      //pg_version
+      expect(responseBody.dependencies.database.version).toBeDefined();
+      expect(responseBody.dependencies.database.version).toContain("16.0");
 
-  //pg_max_conn
-  expect(responseBody.dependencies.database.max_connections).toBeDefined();
-  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+      //pg_max_conn
+      expect(responseBody.dependencies.database.max_connections).toBeDefined();
+      expect(responseBody.dependencies.database.max_connections).toEqual(100);
 
-  //pg_count_conn
-  expect(responseBody.dependencies.database.opened_connections).toBeDefined();
-  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+      //pg_count_conn
+      expect(
+        responseBody.dependencies.database.opened_connections,
+      ).toBeDefined();
+      expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+    });
+  });
 });
