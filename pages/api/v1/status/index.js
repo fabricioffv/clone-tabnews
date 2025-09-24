399736bd71f32e7751ef2e0a47ca9f355353dea2
Fabricio Viapiana <fabriciofviapiana@gmail.com>
2025-09-24 19:02:14 -0300

    refactor: add custom error to `POST` status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,43 +1,56 @@
+import { createRouter } from "next-connect";
 import database from "infra/database.js";
-import { InternalServerError } from "/infra/errors";
+import { InternalServerError, MethodNotAllowedError } from "/infra/errors";
 
-async function status(request, response) {
-  try {
-    const pgVersionQuery = await database.query("SHOW server_version;");
-    const pgVersion = pgVersionQuery.rows[0].server_version;
+const router = createRouter();
 
-    const pgMaxConnQuery = await database.query("SHOW max_connections;");
-    const pgMaxConn = pgMaxConnQuery.rows[0].max_connections;
+router.get(getHandler);
 
-    const databaseName = process.env.POSTGRES_DB;
-    const pgConnCountQuery = await database.query({
-      text: "select count(*)::int from pg_stat_activity WHERE datname = $1",
-      values: [databaseName],
-    });
-    const pgConnCount = pgConnCountQuery.rows[0].count;
+export default router.handler({
+  onNoMatch: onNoMatchHandler,
+  onError: onErrorHandler,
+});
 
-    const updatedAt = new Date().toISOString();
+function onErrorHandler(error, _, res) {
+  const publicErrorObject = new InternalServerError({
+    cause: error,
+  });
 
-    response.status(200).json({
-      updated_at: updatedAt,
-      dependencies: {
-        database: {
-          version: pgVersion,
-          max_connections: parseInt(pgMaxConn),
-          opened_connections: pgConnCount,
-        },
-      },
-    });
-  } catch (error) {
-    const publicErrorObject = new InternalServerError({
-      cause: error,
-    });
+  console.log("\n Erro dentro do catch do next-connect");
+  console.error(publicErrorObject);
 
-    console.log("\n Erro dentro do catch do controller /status");
-    console.error(publicErrorObject);
-
-    response.status(500).json(publicErrorObject);
-  }
+  res.status(500).json(publicErrorObject);
 }
 
-export default status;
+function onNoMatchHandler(_, res) {
+  const publicErrorObject = new MethodNotAllowedError();
+  res.status(publicErrorObject.statusCode).json(publicErrorObject);
+}
+
+async function getHandler(_, response) {
+  const pgVersionQuery = await database.query("SHOW server_version;");
+  const pgVersion = pgVersionQuery.rows[0].server_version;
+
+  const pgMaxConnQuery = await database.query("SHOW max_connections;");
+  const pgMaxConn = pgMaxConnQuery.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const pgConnCountQuery = await database.query({
+    text: "select count(*)::int from pg_stat_activity WHERE datname = $1",
+    values: [databaseName],
+  });
+  const pgConnCount = pgConnCountQuery.rows[0].count;
+
+  const updatedAt = new Date().toISOString();
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: pgVersion,
+        max_connections: parseInt(pgMaxConn),
+        opened_connections: pgConnCount,
+      },
+    },
+  });
+}
