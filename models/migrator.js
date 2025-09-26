1c35ff4ef517c9664d325a33abc0699af64bd0f6
Fabricio Viapiana <fabriciofviapiana@gmail.com>
2025-09-25 22:03:00 -0300

    feat: create migrator model

---
+++ models/migrator.js
@@ -0,0 +1,55 @@
+import database from "infra/database.js";
+import migrationRunner from "node-pg-migrate";
+import path from "node:path";
+
+const defaultMigrationOptions = {
+  dryRun: true,
+  dir: path.resolve("infra", "migrations"),
+  direction: "up",
+  verbose: true,
+  migrationsTable: "pgmigrations",
+};
+
+async function listPendingMigrations() {
+  let dbClient;
+
+  try {
+    dbClient = await database.getNewClient();
+
+    const pendingMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dbClient,
+    });
+
+    return pendingMigrations;
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+async function runPendingMigrations() {
+  let dbClient;
+
+  try {
+    dbClient = await database.getNewClient();
+
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dryRun: false,
+      dbClient,
+    });
+
+    return migratedMigrations;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+const migrator = {
+  listPendingMigrations,
+  runPendingMigrations,
+};
+
+export default migrator;
