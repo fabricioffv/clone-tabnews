d4dbe754ee2278263bc283a6b9301d86fe538579
Fabricio Viapiana <fabriciofviapiana@gmail.com>
2025-10-14 23:06:57 -0300

    feat: enhance Home component with image display

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,26 @@
+import Image from "next/image";
+
 function Home() {
-  return <h1>A sorte segue a coragem!</h1>;
+  return (
+    <>
+      <h1>A sorte segue a coragem!</h1>
+      <div
+        style={{
+          position: "relative",
+          width: "100%",
+          maxWidth: "600px",
+          aspectRatio: "16/9",
+        }}
+      >
+        <Image
+          src="/images/taia.jpg"
+          alt="A sorte segue a coragem!"
+          fill
+          style={{ objectFit: "cover" }}
+        />
+      </div>
+    </>
+  );
 }
 
 export default Home;
