dfe9844be8f9b77440a75ae00713217133a682a3
Fabricio Viapiana <fabriciofviapiana@gmail.com>
2025-10-18 09:52:10 -0300

    chore: remove img

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,7 @@
-import Image from "next/image";
-
 function Home() {
   return (
     <>
       <h1>A sorte segue a coragem!</h1>
-      <div
-        style={{
-          position: "relative",
-          width: "100%",
-          maxWidth: "600px",
-          aspectRatio: "16/9",
-        }}
-      >
-        <Image
-          src="/images/taia.jpg"
-          alt="A sorte segue a coragem!"
-          fill
-          style={{ objectFit: "cover" }}
-        />
-      </div>
     </>
   );
 }
